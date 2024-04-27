a41d0fc0762c0e875bfe095d20f433fa2ed31201
dalvarez-2022277 <dalvarez-2022277@kinal.edu.gt>
2024-04-27 00:18:55 -0600

    Componente funcional Barra escribir tarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import './App.css';
+import { TodoAdd } from './components/TodoAdd';
 
+
+function App() {
+	return (
+		<>
+			<div className='card-to-do'>
+				<h1>Lista de tareas</h1>
+				<div className='add-todo'>
+					<h3>Agregar Tarea</h3>
+					<TodoAdd handleNewTodo />
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
